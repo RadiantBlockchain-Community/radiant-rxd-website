70cb8f5b8e0db6227207f839735043c5de30fbe6
Bitopia <145090506+Bitopia-RXD@users.noreply.github.com>
2026-05-02 18:24:38 +1000

    update Radiant brand guide, add homepage RXD coin supply module and update links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -660,7 +660,7 @@ function resetFocusTabsStyle() {
 }());
 
 
-// RXD live market stats
+// start RXD live market stats
 async function fetchData() {
   try {
       const response = await fetch('https://api.coingecko.com/api/v3/coins/radiant?localization=false&tickers=false&market_data=true&community_data=false&developer_data=false&sparkline=false');
@@ -669,19 +669,40 @@ async function fetchData() {
       const currentPrice = data.market_data.current_price.usd;
       const marketCap = data.market_data.market_cap.usd;
       const totalVolume = data.market_data.total_volume.usd;
+      
+      const circulatingSupply = data.market_data.circulating_supply;
+
+      const MAX_SUPPLY = 21000000000;
+      const remainingSupply = MAX_SUPPLY - Math.floor(circulatingSupply);
 
       document.getElementById('current-price').innerText = `$${currentPrice}`;
       document.getElementById('market-cap').innerText = `$${marketCap.toLocaleString()}`;
       document.getElementById('total-volume').innerText = `$${totalVolume.toLocaleString()}`;
+
+      const circEl = document.getElementById('circ-supply');
+      if (circEl) {
+        const formattedCirc = Math.floor(circulatingSupply).toLocaleString('en-US');
+        circEl.textContent = formattedCirc;
+      }
+
+      const remainingEl = document.getElementById('remaining-supply');
+      if (remainingEl) {
+        const formattedRemaining = remainingSupply.toLocaleString('en-US');
+        remainingEl.textContent = formattedRemaining;
+      }
+
   } catch (error) {
       console.error('Error fetching data:', error);
+      
+      const circEl = document.getElementById('circ-supply');
+      const remainingEl = document.getElementById('remaining-supply');
+      if (circEl) circEl.textContent = '—';
+      if (remainingEl) remainingEl.textContent = '—';
   }
 }
 
-// Fetch data on page load
 fetchData();
 
-// Refresh data every 5 minutes
 setInterval(fetchData, 300000);
 
 // end RXD live market stats
